6c4c42ef764e7f484169898a281f6a23a612a390
Nihuan <nihuan@isoubu.com>
2018-07-27 11:01:53 +0800

    无报价采购刷新频率修改

--- app/Tasks/RefreshNoQuoteTask.php
+++ app/Tasks/RefreshNoQuoteTask.php
@@ -11,6 +11,7 @@ namespace App\Tasks;
 
 use App\Models\Data\BuyAttributeData;
 use App\Models\Entity\Buy;
+use SebastianBergmann\CodeCoverage\Report\PHP;
 use Swoft\Bean\Annotation\Inject;
 use Swoft\Task\Bean\Annotation\Scheduled;
 use Swoft\Task\Bean\Annotation\Task;
@@ -30,16 +31,19 @@ class RefreshNoQuoteTask
     private $buyAttrData;
 
     /**
-     * 热门推送标签统计
+     * 热门推送标签统计,每小时执行一次
      * @author Nihuan
-     * @Scheduled(cron="*\/1 * * * * *")
+     * @Scheduled(cron="0 0 * * * *")
      */
     public function refreshTask()
     {
         $prev_time = strtotime('-8 hour');
+        $last_time = strtotime('-9 hour');
         $refresh_prev = strtotime('-30 minute');
+        $now_time = date('Y-m-d H:i:s');
         $buyRes = Buy::findAll([
-            'add_time' => $prev_time,
+            ['add_time','>',$last_time],
+            ['add_time','<=',$prev_time],
             'is_audit' => 0,
             ['refresh_time','>=',$refresh_prev]
         ],
@@ -52,6 +56,7 @@ class RefreshNoQuoteTask
                     $buy_ids[] = $buy['buyId'];
                 }
             }
+            echo "[$now_time] 共刷新采购条数:" . count($buy_ids) . PHP_EOL;
             if(!empty($buy_ids)){
                 Buy::updateAll(['refresh_time' => time(),'alter_time' => time()],['buy_id' => $buy_ids])->getResult();
             }
