66cae50172c88efbad7e175c3c555707c2b7d914
Nihuan <nihuan@isoubu.com>
2018-09-14 13:15:10 +0800

    消息发送队列调整

--- app/Tasks/IMSmsSendQueueTask.php
+++ app/Tasks/IMSmsSendQueueTask.php
@@ -36,10 +36,15 @@ class IMSmsSendQueueTask
      */
     public function sendTask()
     {
+        $return_msg = '';
         $now_time = time();
         $queue = $this->redis->exists($this->queue_key);
         if($queue){
             $msg = $this->redis->lPop($this->queue_key);
+            $secret_code = md5($msg);
+            if($this->redis->exists($secret_code)){
+                $return_msg = '发送记录已存在，跳过';
+            }
             $msg_info = json_decode($msg,true);
             if($msg_info['timedTask'] > 0 && $msg_info['timedTask'] < $now_time){
                 //如果设置了定时 & 发送时间没有到，重新写入队列，等待轮循
@@ -47,7 +52,11 @@ class IMSmsSendQueueTask
                 $this->redis->rPush($this->queue_key,$msg_json);
             }else{
                 sendInstantMessaging($msg_info['fromId'], (string)$msg_info['targetId'], json_encode($msg_info['msgExtra']));
+                $this->redis->set($secret_code,$msg);
+                $this->redis->expire($secret_code,180);
+                $return_msg = '发送成功';
             }
         }
+        return [$return_msg];
     }
 }
