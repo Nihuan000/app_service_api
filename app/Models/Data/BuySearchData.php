9413faf456441a7bec91b4930d413730137a6816
Nobody <nobody@gmail.com>
2019-07-06 10:59:10 +0800

    标签缓存时间修改

--- app/Models/Data/BuySearchData.php
+++ app/Models/Data/BuySearchData.php
@@ -59,10 +59,11 @@ class BuySearchData
         $filter = $this->baseFilter();
         $user_tag_string = $this->redis->get('user_subscription_tag:' . $params['user_id']);
         $user_tag_list = json_decode($user_tag_string,true);
-        if(empty($user_tag_list)){
+        $ttl = $this->redis->ttl('user_subscription_tag:' . $params['user_id']);
+        if(empty($user_tag_list) || $ttl < 0){
             $user_tag_list = $this->userDao->getUserTagByUid($params['user_id']);
             if(!empty($user_tag_list)){
-                $this->redis->set('user_subscription_tag:' . $params['user_id'],json_encode($user_tag_list));
+                $this->redis->setex('user_subscription_tag:' . $params['user_id'],7*24*3600,json_encode($user_tag_list));
             }
         }
         $product_terms = [];
@@ -123,7 +124,7 @@ class BuySearchData
         if(!empty($product_terms)){
             $new_product = array_unique($product_terms);
             foreach ($new_product as $item) {
-                $parent_terms_list[] = ['term' => ['labels_normalized' => $item]];
+                $parent_terms_list[] = ['term' => ['labels' => $item]];
             }
         }
 
