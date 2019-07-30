bbaa1f65580dc0a64acbcd1ee960651eb3e8a8f3
dailifeng <dailifeng@isoubu.com>
2019-07-30 14:01:50 +0800

    保证金存的消息

--- app/Tasks/UserTask.php
+++ app/Tasks/UserTask.php
@@ -52,6 +52,11 @@ class UserTask{
     private $safe_price_msg_queue = 'safe_price_msg_queue';
 
     /**
+     * @var string
+     */
+    private $safe_price_send_msg_msg_queue = 'safe_price_send_msg_msg_queue';
+
+    /**
      * 用户保证金提取操作
      * 每分钟26秒执行一次
      * @Scheduled(cron="26 * * * * *")
@@ -60,7 +65,7 @@ class UserTask{
     public function safePriceTask()
     {
         $end_time = time();
-        $start_time = time()-3600;
+        $start_time = time()-60;
         $safe_price_list = $this->redis->zRangeByScore($this->safe_price_msg_queue, $start_time, $end_time);
         if(!empty($safe_price_list)){
             write_log(3,"保证金提取的用户数组".json_encode($safe_price_list));
@@ -83,9 +88,10 @@ class UserTask{
                     $res['extra']['content'] = $msg;
                     $res['extra']['msgContent'] = $msg;
                     sendInstantMessaging('1',(string)$user_id,json_encode($res['extra']));
-                    if($this->userData->getSetting('SEND_SMS') == 1 && !empty($sms)){
+                    if($this->userData->getSetting('SEND_SMS') == 1 && !empty($msg)){
                         $user_info = $this->userData->getUserInfo($user_id);
                         if(!empty($user_info)){
+                            $msg .= " 退订回T";
                             //sendSms($user_info['phone'],$msg,2,2);
                         }
                     }
@@ -99,6 +105,39 @@ class UserTask{
                     $next_time = $time;
                     $this->redis->zAdd($this->safe_price_msg_queue, $next_time, $user_id);
                 }
+            }
+        }
+    }
+
+    /**
+     * 用户保证金提取操作
+     * 每分钟56秒执行一次
+     * @Scheduled(cron="56 * * * * *")
+     * @throws DbException
+     */
+    public function safePriceSendMsgTask()
+    {
+        $end_time = time();
+        $start_time = time()-60;
+        $safe_price_msg_list = $this->redis->zRangeByScore($this->safe_price_send_msg_msg_queue, $start_time, $end_time);
+        if(!empty($safe_price_msg_list)){
+            write_log(3,"保证金消息发送的用户数组".json_encode($safe_price_msg_list));
+            foreach ($safe_price_msg_list as $key => $value) {
+                $this->redis->zDelete($this->safe_price_send_msg_msg_queue, $value);
+            }
+            $time = date('Y-m-d H:i:s', time());
+            foreach ($safe_price_msg_list as $key => $value) {
+                $user_id = (int)$value;
+                Log::info("用户id:{$user_id}在{$time}保证金消息发送开始");
+                write_log(3,"用户id:{$user_id}在{$time}保证金消息发送开始");
+                $msg = "根据搜布官方统计，每上传一个产品，将增加12%的访客咨询，查看店铺-发布产品，即可轻松上传。";
+                $res = []; 
+                $res['extra']['title'] = '温馨提示';
+                $res['extra']['content'] = $msg;
+                $res['extra']['msgContent'] = $msg;
+                sendInstantMessaging('1',(string)$user_id,json_encode($res['extra']));
+                Log::info("用户id:{$user_id}保证金消息发送完成");
+                write_log(3,"用户id:{$user_id}保证金消息发送完成");
             }
         }
     }
